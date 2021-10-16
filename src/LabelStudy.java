81ad2b487d851cff42be54f5252fc616e9cb4978
Lsapee <dlworhks94@gmail.com>
2021-10-16 22:51:02 +0900

    라벨 텍스트

--- src/LabelStudy.java
+++ src/LabelStudy.java
@@ -1,6 +1,7 @@
 
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
@@ -11,7 +12,21 @@ public class LabelStudy extends Application {
 	public void start(Stage primaryStage) {
 		Label lb = new Label("study");
 		
-		VBox vb = new VBox(lb);
+		Button bt = new Button("클릭");
+		Button bt2 = new Button("클릭");
+		
+		bt.setOnAction(e->{
+			lb.setText("안녕하세요");
+		});
+		
+		bt2.setOnAction(e->{
+			String a = lb.getText();
+			System.out.println("라벨 텍스트 : " + a);
+		});
+		
+		VBox vb = new VBox(lb,bt,bt2);
+		
+	
 		
 		Scene scene = new Scene(vb,500,500);
 		
